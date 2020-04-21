e86b5bc6cbd55a9ecad7133de72315d27b0e0a47
robbyyt <49028058+robbyyt@users.noreply.github.com>
2020-04-21 21:37:05 +0300

    Register Method

--- src/app/session/services/session/session.service.ts
+++ src/app/session/services/session/session.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { ILoginRequestPayload, ILoginResponse } from '../../models';
+import { ILoginRequestPayload, ILoginResponse, IRegisterPayload } from '../../models';
 import { environment } from 'src/environments/environment';
 import { map } from 'rxjs/operators';
 @Injectable({
@@ -18,11 +18,15 @@ export class SessionService {
     return this.http.post<ILoginResponse>(`${this.endpoint}/login`, payload).pipe(
       map(user => {
         if ( user.success ) {
-          localStorage.setItem('user',JSON.stringify(user))
-          localStorage.setItem('token',user.token);
+          localStorage.setItem('user', JSON.stringify(user));
+          localStorage.setItem('token', user.token);
           return user;
         }
       })
-    ); 
+    );
+  }
+
+  public register(payload: IRegisterPayload): Observable<any> {
+    return this.http.post<any>(`${this.endpoint}/register`, payload);
   }
 }
