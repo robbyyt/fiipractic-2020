c90e494c1ac824db61d11c5fb1a6e0d2957863a1
alexcambose <alexcambose@yahoo.com>
2020-05-04 21:45:24 +0300

    Code style fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {LoginComponent, RegisterComponent, ResetPasswordComponent} from './session/pages';
+import { LoginComponent, RegisterComponent, ResetPasswordComponent } from './session/pages';
 
 const routes: Routes = [
   {
--- src/app/session/pages/reset-password/reset-password.component.ts
+++ src/app/session/pages/reset-password/reset-password.component.ts
@@ -9,9 +9,6 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 export class ResetPasswordComponent implements OnInit {
   public resetPasswordForm: FormGroup;
 
-  constructor() {
-  }
-
   ngOnInit(): void {
     this.resetPasswordForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
