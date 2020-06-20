c13e7a9b7717d68b5ce4b4b063cd8e6080c35eb1
robbyyt <49028058+robbyyt@users.noreply.github.com>
2020-06-20 20:31:14 +0300

    Solved PR issues

--- src/app/shared/components/mobile-menu/mobile-menu.component.ts
+++ src/app/shared/components/mobile-menu/mobile-menu.component.ts
@@ -1,17 +1,12 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-mobile-menu',
   templateUrl: './mobile-menu.component.html',
   styleUrls: ['./mobile-menu.component.scss']
 })
-export class MobileMenuComponent implements OnInit {
+export class MobileMenuComponent {
   @Output() sidenavClose = new EventEmitter();
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
   public closeSidenav(): void {
     this.sidenavClose.emit();
